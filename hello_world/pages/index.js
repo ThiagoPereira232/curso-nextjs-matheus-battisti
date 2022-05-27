6eb79f5494422dd925cff47904a9cadf5f3299f0
ThiagoPereira232 <pereira.tp.thiago@gmail.com>
2022-05-26 22:10:58 -0300

    images

--- hello_world/pages/index.js
+++ hello_world/pages/index.js
@@ -1,6 +1,7 @@
 import styles from '../styles/Home.module.css'
 
 import Head from 'next/head'
+import Image from 'next/image'
 
 export default function Home() {
   return (
@@ -12,6 +13,7 @@ export default function Home() {
       </Head>
       <div className={styles.container}>
         <h1 className={styles.title}>Hello World nextjs</h1>
+        <Image src="/images/city.jpg" width="400px" height="600px" alt="Cidade a noite"/>
       </div>
     </>
   )
