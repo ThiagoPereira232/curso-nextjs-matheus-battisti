c7713b5dc1e206d61301338922dd5a096bc469d6
ThiagoPereira232 <pereira.tp.thiago@gmail.com>
2022-05-27 10:44:46 -0300

    Create 404.js

---
+++ dynamic_with_datas/pages/404.js
@@ -0,0 +1,10 @@
+import Link from 'next/link'
+
+export default function NotFound(){
+    return (
+        <>
+            <h1>Página não encontrada</h1>
+            <Link href="/"><a>Voltar para a página inicial</a></Link>
+        </>
+    )
+}
