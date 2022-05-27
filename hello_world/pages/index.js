814ad0a65998516d43362364cc71c22566b0fd59
ThiagoPereira232 <pereira.tp.thiago@gmail.com>
2022-05-26 21:01:16 -0300

    link

--- hello_world/pages/index.js
+++ hello_world/pages/index.js
@@ -1,10 +1,23 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import Link from 'next/link'
 
 export default function Home() {
   return (
     <div className={styles.container}>
+      <ul>
+        <li>
+          <Link href="/products">
+            <a>Produtos</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/about">
+            <a>Sobre</a>
+          </Link>
+        </li>
+      </ul>
       <h1>Hello World nextjs</h1>
     </div>
   )
